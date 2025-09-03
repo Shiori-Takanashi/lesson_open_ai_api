d86ea07aa5bbed06c310f808fc597658b80b16cf
Takanashi <nao69alter@gmail.com>
2025-09-04 00:34:02 +0900

    Progressing: I am sleepy. I should have a break.

--- utils/get_path.py
+++ utils/get_path.py
@@ -18,3 +18,33 @@ def find_project_root(start: Path | None = None) -> Path:
             return path
 
     raise FileNotFoundError("プロジェクトルート (.git) が見つかりませんでした")
+
+def validate_dirname(child_dirname: str) -> str:
+    invalid_chars = ["/"]
+
+    for invalid_char in invalid_chars:
+        if invalid_char in child_dirname:
+            raise ValueError(f"{child_dirname} is invalid.")
+        else:
+            pass
+
+    return child_dirname
+
+
+
+def ensure_outdir_from_dirname(child_dirname: str) -> None:
+    if not child_dirname:
+        raise ValueError(f"child_dirname is '' .")
+
+    outdirpath: Path | None = None
+
+    rootpath = find_project_root()
+    valid_child_dirname = validate_dirname(child_dirname)
+    out_dirpath = rootpath / child_dirname
+    out_dirpath.mkdir(exist_ok=True)
+
+    return out_dirpath
+
+def get_outdir_from_dirname() -> Path:
+    out_dirpath = ensure_outdir_form_dirname()
+    return out_dirpath
